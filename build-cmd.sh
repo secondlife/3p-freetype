bdf1e4a09fd862df585eb59c44d47fd9cac381b8
Rye <rye@lindenlab.com>
2024-11-21 21:26:32 -0500

    Add build support for macOS universal libraries

--- build-cmd.sh
+++ build-cmd.sh
@@ -66,48 +66,49 @@ pushd "$FREETYPELIB_SOURCE_DIR"
         ;;
 
         darwin*)
-            # Darwin build environment at Linden is also pre-polluted like Linux
-            # and that affects colladadom builds.  Here are some of the env vars
-            # to look out for:
-            #
-            # AUTOBUILD             GROUPS              LD_LIBRARY_PATH         SIGN
-            # arch                  branch              build_*                 changeset
-            # helper                here                prefix                  release
-            # repo                  root                run_tests               suffix
-
-            opts="${TARGET_OPTS:--arch $AUTOBUILD_CONFIGURE_ARCH $LL_BUILD_RELEASE}"
-            plainopts="$(remove_cxxstd $opts)"
-
             export MACOSX_DEPLOYMENT_TARGET="$LL_BUILD_DARWIN_DEPLOY_TARGET"
 
-            mkdir -p "build"
-            pushd "build"
-                cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS:BOOL=OFF \
-                    -DCMAKE_C_FLAGS="$plainopts" \
-                    -DCMAKE_CXX_FLAGS="$opts" \
-                    -DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 \
-                    -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
-                    -DCMAKE_MACOSX_RPATH=YES \
-                    -DCMAKE_INSTALL_PREFIX="$stage" \
-                    -DCMAKE_INSTALL_LIBDIR="$stage/lib/release" \
-                    -DFT_REQUIRE_ZLIB=ON \
-                    -DFT_REQUIRE_PNG=ON \
-                    -DFT_DISABLE_HARFBUZZ=ON \
-                    -DFT_DISABLE_BZIP2=ON \
-                    -DFT_DISABLE_BROTLI=ON \
-                    -DPNG_PNG_INCLUDE_DIR="${stage}/packages/include/libpng16/" \
-                    -DPNG_LIBRARY="${stage}/packages/lib/release/libpng16.a" \
-                    -DZLIB_INCLUDE_DIR="${stage}/packages/include/zlib-ng/" \
-                    -DZLIB_LIBRARY="${stage}/packages/lib/release/libz.a"
+            for arch in x86_64 arm64 ; do
+                ARCH_ARGS="-arch $arch"
+                opts="${TARGET_OPTS:-$ARCH_ARGS $LL_BUILD_RELEASE}"
+                cc_opts="$(remove_cxxstd $opts)"
+                ld_opts="$ARCH_ARGS"
 
-                cmake --build . --config Release
-                cmake --install . --config Release
+                mkdir -p "build_$arch"
+                pushd "build_$arch"
+                    CFLAGS="$cc_opts" \
+                    CXXFLAGS="$opts" \
+                    LDFLAGS="$ld_opts" \
+                    cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS:BOOL=OFF \
+                        -DCMAKE_C_FLAGS="$cc_opts" \
+                        -DCMAKE_CXX_FLAGS="$opts" \
+                        -DCMAKE_OSX_ARCHITECTURES:STRING="$arch" \
+                        -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+                        -DCMAKE_MACOSX_RPATH=YES \
+                        -DCMAKE_INSTALL_PREFIX="$stage" \
+                        -DCMAKE_INSTALL_LIBDIR="$stage/lib/release/$arch" \
+                        -DFT_REQUIRE_ZLIB=ON \
+                        -DFT_REQUIRE_PNG=ON \
+                        -DFT_DISABLE_HARFBUZZ=ON \
+                        -DFT_DISABLE_BZIP2=ON \
+                        -DFT_DISABLE_BROTLI=ON \
+                        -DPNG_PNG_INCLUDE_DIR="${stage}/packages/include/libpng16/" \
+                        -DPNG_LIBRARY="${stage}/packages/lib/release/libpng16.a" \
+                        -DZLIB_INCLUDE_DIR="${stage}/packages/include/zlib-ng/" \
+                        -DZLIB_LIBRARY="${stage}/packages/lib/release/libz.a"
 
-                # conditionally run unit tests
-                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                    ctest -C Release
-                fi
-            popd
+                    cmake --build . --config Release
+                    cmake --install . --config Release
+
+                    # conditionally run unit tests
+                    if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                        ctest -C Release
+                    fi
+                popd
+            done
+
+            # Create universal library
+            lipo -create -output "$stage/lib/release/libfreetype.a" "$stage/lib/release/x86_64/libfreetype.a" "$stage/lib/release/arm64/libfreetype.a"
         ;;
 
         linux*)
